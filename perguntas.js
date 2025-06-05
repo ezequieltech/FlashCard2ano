7eba3ff70ddf9f414d9c70c4493e21b29200275c
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2025-06-05 19:44:28 -0300

    x

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é a cidade de Paris.'
+)
+criaCartao(
+    'História',
+    'Em que ano os portugueses chegaram ao território brasileiro?',
+    'Os portugueses chegaram ao Brasil em 1500.'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'Roma é a capital da Itália.'
+)
